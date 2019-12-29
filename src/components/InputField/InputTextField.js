2351d27f46d99210bfadcc3dd89fe88987f6bac6
Milagros Rodriguez <milagros@krowdy.co>
2019-12-28 20:19:05 -0500

    feat: adding proptypes to input text component

--- src/components/InputField/InputTextField.js
+++ src/components/InputField/InputTextField.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from '../styles/inputField.module.css';
+import PropTypes from '@dhis2/prop-types';
 
-const InputTextField = (inputData) => {
+/* const InputTextField = (inputData) => {
     return (
         <div className={styles.input_container}>
             <label className={styles.input_container_label}>
@@ -12,13 +13,49 @@ const InputTextField = (inputData) => {
                     <input
                         name={inputData.name}
                         maxLength={inputData.length}
-                        className={styles.input_field}
+                        className={inputData.error ? styles.input_error + ' ' + styles.input_field : styles.input_field}
                         value={inputData.value}
-                        onChange={inputData.onChange} />
+                        onChange={inputData.onChange}
+                        onBlur = {inputData.onBlur}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+} */
+
+const InputTextField = ({ label, name, length, value, onChange, onBlur, error }) => {
+    console.log({ label: label, name: name, length: length, value: value, onChange: onChange, onBlur: onBlur, error })
+    return (
+        <div className={styles.input_container}>
+            <label className={styles.input_container_label}>
+                <span> {label} </span>
+            </label>
+            <div className={styles.input_container_text}>
+                <div className={styles.input_field_container}>
+                    <input
+                        name={name}
+                        maxLength={length}
+                        className={error ? styles.input_error + ' ' + styles.input_field : styles.input_field}
+                        value={value}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                    />
                 </div>
             </div>
         </div>
     )
 }
+
+InputTextField.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.node.isRequired,
+    onChange: PropTypes.func.isRequired,
+    error: PropTypes.bool,
+    length: PropTypes.string,
+    onBlur: PropTypes.func,
+}
+
 
 export default InputTextField
