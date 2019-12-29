e4655cfcd9927d97fe93db91aab40503e0059a41
Milagros Rodriguez <milagros@krowdy.co>
2019-12-28 20:22:23 -0500

    fix: cleaning input component

--- src/components/InputField/InputTextField.js
+++ src/components/InputField/InputTextField.js
@@ -2,30 +2,7 @@ import React from 'react'
 import styles from '../styles/inputField.module.css';
 import PropTypes from '@dhis2/prop-types';
 
-/* const InputTextField = (inputData) => {
-    return (
-        <div className={styles.input_container}>
-            <label className={styles.input_container_label}>
-                <span> {inputData.label} </span>
-            </label>
-            <div className={styles.input_container_text}>
-                <div className={styles.input_field_container}>
-                    <input
-                        name={inputData.name}
-                        maxLength={inputData.length}
-                        className={inputData.error ? styles.input_error + ' ' + styles.input_field : styles.input_field}
-                        value={inputData.value}
-                        onChange={inputData.onChange}
-                        onBlur = {inputData.onBlur}
-                    />
-                </div>
-            </div>
-        </div>
-    )
-} */
-
 const InputTextField = ({ label, name, length, value, onChange, onBlur, error }) => {
-    console.log({ label: label, name: name, length: length, value: value, onChange: onChange, onBlur: onBlur, error })
     return (
         <div className={styles.input_container}>
             <label className={styles.input_container_label}>
